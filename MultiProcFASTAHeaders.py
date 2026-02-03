2f598ff1bc5151fb33079d55ca407bab54a4cd63
Lauren Sabo <53955594+laurengsabo@users.noreply.github.com>
2026-02-03 16:00:06 -0500

    updated mid-script documentation

--- MultiProcFASTAHeaders.py
+++ MultiProcFASTAHeaders.py
@@ -1,40 +1,35 @@
-### Information
-    # @author Lauren Sabo
-    # @version 1.0
-    # notes are for Nikesh Kumar
+#!/usr/bin/env python3
+"""
+Description: Script to split FASTA contigs and process them using multiprocessing.
+@author: Lauren Sabo | @version: 2.0
+Notes for: Nikesh Kumar
+"""
 
+import os
+import sys
 import subprocess as sp
-    # Using "from multiprocessing import Process" lets you just type "Process" into the splitJobs Function below, 
-        # if you were to just type "import multiprocessing", you would have to type "multiprocessing.Process(...)" in
-        # the function instead.
+    # Using "from multiprocessing import Process" you can type "Process" into the splitJobs Function below, 
+        # if you were to just type "import multiprocessing", you would have to type "multiprocessing.Process(...)" in the function instead.
 from multiprocessing import Process
 from pyfaidx import Fasta
 
-### SPLICE FUNCTION
-    # This function will take the Fasta file and do the following:
-    #       1. Puts all items that do not start with "NC" (AKA all contigs) into a list called "contigKeys"
-    #       2. contigKeys will then be split up into lists with a size of the desired run size
-    #       3. All of the lists will then be put into a list, and this is returned
-def splice(fastaLoc, size):
+def splice(fasta_loc, size):
+    """
+    1. Filter out 'NC' sequences (Chromosomes) to keep only contigs.
+    2. Chunk the list into sub-lists of a specific 'run size'.
+    """
     fasta = Fasta(fastaLoc)
+    # 1. Filtering
+    contig_keys = [c for c in fasta.keys() if not c.startswith('NC')]
+
+    # 2 & 3. Chunking (List Comprehension)
+    return [contig_keys[i : i + int(size)] for i in range(0, len(contig_keys), int(size))]
     
-    # 1
-    contigKeys = []
-    for contig in fasta.keys():
-        if contig.startswith('NC'):
-            continue
-        contigKeys.append(contig)
-
-    # 2 & 3
-    return [contigKeys[i:int(i+size)] for i in range(0, len(contigKeys), int(size))]
-
-
-### MAKE FILE FUNCTION
-    # For each of the contigs in the list provided, a file will be made within a desired directory.
-    # The name of each file will be the name of each contig.
-def makeFile(contig, contigKeyFileDest):
-    sp.call(["touch", contigKeyFileDest + "/" + contig])
-
+def make_file(contig, output_dir):
+    """For each contig, creates an empty file named after the contig within the chosen dir."""
+    # Using os.path.join for cross-platform safety
+    dest_path = os.path.join(output_dir, contig)
+    sp.call(["touch", dest_path])
 
 ### SPLIT JOBS FUNCTION
     # 1. The function starts off by using the SPLICE Function and saving the lists of lists into a variable called "scopes".
@@ -52,29 +47,51 @@ def makeFile(contig, contigKeyFileDest):
     #           now we run it. Since it is a for-loop, we're going to run each of the scopes sequentially, and the n-number
     #           of items within each scope will run together.
     #       
-def splitJobs(size, fastaLoc, contigKeyFileLoc, fileName):
-    # 1
-    scopes = splice(fastaLoc, size)
+def split_jobs(size, fasta_loc, destination_root, folder_name):
+    """
+    Orchestrates the multiprocessing logic.
+    Groups jobs into 'batches' based on size to avoid overwhelming the CPU.
+    """
+    # 1. Get the chunked list of contigs
+    scopes = splice(fasta_loc, size)
 
-    # 2
-    sp.run(["mkdir", contigKeyFileLoc + "/" + fileName])
+    # 2. Setup output directory using the 'os' module best practices
+    output_path = os.path.join(destination_root, folder_name)
+    os.makedirs(output_path, exist_ok=True)
 
-    # 3A
-    jobs = []
+    # 3. Multiprocessing Magic
     for scope in scopes:
+        jobs = []
+        # Start a batch of processes
         for contig in scope:
-            j = Process(target=makeFile, args=(contig, contigKeyFileLoc + "/" + fileName))
-            jobs.append(j)
-            j.start()
+            p = Process(target=make_file, args=(contig, output_path))
+            jobs.append(p)
+            p.start()
 
-        i = 0
+        # Wait for this specific batch to finish before starting the next
+        # This prevents the computer from trying to run 10,000 things at once
         for j in jobs:
             j.join()
-            i += 1
+            
 
-        del jobs[:]
+def main():
+    # Check if the user provided all 4 required arguments
+    if len(sys.argv) != 5:
+        print("Usage: python script.py <run_size> <fasta_path> <output_root> <project_name>")
+        print("Example: python script.py 22 ./data.fna ./results my_experiment")
+        sys.exit(1) # Exit with an error code
 
-        
+    # Assigning arguments to descriptive variables
+    run_size       = sys.argv[1]
+    fasta_path     = sys.argv[2]
+    output_root    = sys.argv[3]
+    project_name   = sys.argv[4]
+
+    print(f"Starting job: {project_name} with batch size {run_size}...")
+    
+    split_jobs(run_size, fasta_path, output_root, project_name)
+    
+    print("Process complete.")
 
 if __name__ == "__main__":
-    splitJobs(22, '/Users/laurengsabo/Documents/mcgrath/2023/GCF_000238955.4_M_zebra_UMD2a_genomic.fna', "/Users/laurengsabo/Documents/mcgrath/2023/VSCode", "contigTrash")
+    main()
